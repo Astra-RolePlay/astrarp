83e389a335598f7066302989318a1202319a151f
ulasdemirci0 <ulaskarasu54@gmail.com>
2024-02-02 06:12:53 +0000

    fishing güncellendi - loadingscreen değişti - illenium appereance taşındı

--- files/resources/[QBcore]/[Jobs]/qb-fishing/sql.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `ak4y_fishing` (
-  `citizenid` varchar(255) DEFAULT NULL,
-  `currentXP` int(11) DEFAULT NULL,
-  `tasks` longtext DEFAULT NULL,
-  `time` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
