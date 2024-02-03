a5dc90bfdfe7692da133b982a49c4213a9fba1f8
ulasdemirci0 <ulaskarasu54@gmail.com>
2024-02-03 23:23:24 +0000

    tv scripti güncellendi

--- files/resources/[ELUSED]/elused-combat/client.lua
+++ files/resources/[ELUSED]/elused-combat/client.lua
@@ -24,48 +24,57 @@ end)
 
 function Display(id, crds, identifier, reason)
     local displaying = true
-
-    Citizen.CreateThread(function()
-        Wait(Config.DrawingTime)
-        displaying = false
-    end)
-	
     Citizen.CreateThread(function()
         while displaying do
             Wait(5)
             local pcoords = GetEntityCoords(PlayerPedId())
-            if GetDistanceBetweenCoords(crds.x, crds.y, crds.z, pcoords.x, pcoords.y, pcoords.z, true) < 15.0 and show3DText then
+            if GetDistanceBetweenCoords(crds, pcoords.x, pcoords.y, pcoords.z, true) < 15.0 and show3DText then
                 DrawText3DSecond(crds.x, crds.y, crds.z+0.15, "Player Left Game")
-                DrawText3D(crds.x, crds.y, crds.z, "ID: "..id.." ("..identifier..")\nReason: "..reason)
+                DrawText3D(crds.x, crds.y, crds.z, "ID: " .. id .. " (" .. identifier .. ")\nReason: " .. reason)
+                UdeMarker3D(crds.x,crds.y,crds.z+0.50)
             else
                 Citizen.Wait(2000)
             end
         end
     end)
+    Citizen.CreateThread(function()
+        Wait(Config.DrawingTime)
+        displaying = false
+    end)
 end
 
-function DrawText3DSecond(x,y,z, text)
-    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
-    local px,py,pz=table.unpack(GetGameplayCamCoords())
+ 
+function DrawText3DSecond(x, y, z, text)
+    local onScreen, _x, _y = World3dToScreen2d(x, y, z)
+    local px, py, pz = table.unpack(GetGameplayCamCoords())
     SetTextScale(0.45, 0.45)
-    SetTextFont(4)
+    SetTextFont(0)
     SetTextProportional(1)
     SetTextColour(Config.AlertTextColor.r, Config.AlertTextColor.g, Config.AlertTextColor.b, 215)
     SetTextEntry("STRING")
     SetTextCentre(1)
     AddTextComponentString(text)
-    DrawText(_x,_y)
+    DrawText(_x, _y)
+    -- local factor = (string.len(text)) / 250
+    -- DrawRect(_x, _y + 0.0125, 0.015 + factor, 0.03, 0, 0, 0, 75)
 end
 
-function DrawText3D(x,y,z, text)
-    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
-    local px,py,pz=table.unpack(GetGameplayCamCoords())
+function DrawText3D(x, y, z, text)
+    local onScreen, _x, _y = World3dToScreen2d(x, y, z)
+    local px, py, pz = table.unpack(GetGameplayCamCoords())
     SetTextScale(0.45, 0.45)
-    SetTextFont(4)
+    SetTextFont(0)
     SetTextProportional(1)
     SetTextColour(Config.TextColor.r, Config.TextColor.g, Config.TextColor.b, 215)
     SetTextEntry("STRING")
     SetTextCentre(1)
     AddTextComponentString(text)
-    DrawText(_x,_y)
+    DrawText(_x, _y)
+    local factor = (string.len(text)) / 250
+    DrawRect(_x, _y + 0.0125, 0.015 + factor, 0.03, 0, 0, 0, 75)
+end
+
+
+function UdeMarker3D(x,y,z)
+    DrawMarker(2,x,y,z,0.0,0.0,0.0,0.0,0.0,0.0,0.3,0.3,0.3,100,0,254,150,0,0,0,0,0,0,0)
 end
--- files/resources/[ELUSED]/elused-combat/config.lua
+++ files/resources/[ELUSED]/elused-combat/config.lua
@@ -4,7 +4,7 @@ Config.DrawingTime = 20*1000 --20 seconds
 Config.TextColor = {r=255, g=255,b=255} -- WHITE (Player Data)
 Config.AlertTextColor = {r=255, g=0, b=0} -- RED (Player Left Game)
 Config.LogSystem = true
-Config.UseSteam = true -- If False then use R* License
+Config.UseSteam = false -- If False then use R* License
 Config.LogBotName = "elused-combat"
 Config.AutoDisableDrawing = true
 Config.AutoDisableDrawingTime = 15000
