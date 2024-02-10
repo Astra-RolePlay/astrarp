3e7193ab25140d703a1661ced7a016a8a8475bd3
ulasdemirci0 <ulaskarasu54@gmail.com>
2024-02-10 18:52:33 +0000

    npc'ler aktif edild.

--- files/resources/[ELUSED]/ed-npc/config/NPCedit.lua
+++ files/resources/[ELUSED]/ed-npc/config/NPCedit.lua
@@ -6,18 +6,18 @@ ss_enable_crowd_density = true
 ss_enable_police_ignore_player = true
 ss_enable_no_wanted_level = true
 ss_enable_everyone_ignores_player = true
-ss_enable_random_cop_spawn = true -- This will set SetCreateRandomCopsNotOnScenarios and SetCreateRandomCops to what you picked.
-ss_enable_random_boat_spawn = true
+ss_enable_random_cop_spawn = false -- This will set SetCreateRandomCopsNotOnScenarios and SetCreateRandomCops to what you picked.
+ss_enable_random_boat_spawn = false
 ss_enable_garbage_truck_spawn = true
 
 -- Maximum traffic density for all players. Default is 0.9999
-ss_traffic_max_density = 0
+ss_traffic_max_density = 0.33
 
 -- Enable scaling density by players. (More Players = Less AI Traffic, with a absolute minimum setting of 0.5)
 ss_enable_traffic_scaling = true
 
 -- Maximum crowd density for all players. Default is 0.9999
-ss_crowd_max_density = 0
+ss_crowd_max_density = 0.33
 
 -- Enable scaling density by players. (More Players = Less AI Pedestrians, with a absolute minimum setting of 0.5)
 ss_enable_crowd_scaling = true
